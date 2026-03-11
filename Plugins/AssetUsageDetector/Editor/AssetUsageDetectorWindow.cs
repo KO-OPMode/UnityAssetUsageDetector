6b0bf5f730daf6a31a6293a27988fa6c5a6033be
Jon Gill <jonagill@gmail.com>
2026-03-11 11:07:53 -0700

    Update Search for References MenuItem priority

--- Plugins/AssetUsageDetector/Editor/AssetUsageDetectorWindow.cs
+++ Plugins/AssetUsageDetector/Editor/AssetUsageDetectorWindow.cs
@@ -197,7 +197,7 @@ namespace AssetUsageDetectorNamespace
 		// Quickly initiate search for the selected assets
 		[MenuItem( "GameObject/Search for References/This Object Only", priority = 49 )]
 		// ys custom start -- adjust priority to group with other search tools
-		[MenuItem( "Assets/Search for References", priority = 1000 )]
+		[MenuItem( "Assets/Search for References", priority = 99 )]
 		// ys custom end
 		private static void SearchSelectedAssetReferences( MenuCommand command )
 		{
